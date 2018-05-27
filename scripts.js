7ff250de232b509bb57eade2fd4a741028c6df13
Carlos Ferreira <juniorcaf30@gmail.com>
2018-05-27 17:49:02 -0300

    responsive and effects

--- scripts.js
+++ scripts.js
@@ -1,10 +1,37 @@
-let efeito=document.querySelectorAll('a');
-// funcao do hover
+let efeito = document.querySelectorAll('.contatoLink');
+let buttonEnter = document.querySelector('#botaoEntrar');
+let buttonCad = document.querySelector('#botaoCad');
+let image = document.querySelectorAll('.imagens-section');
+// funcao do hover links
 for(let i=0; i<efeito.length;i++){
     efeito[i].onmouseenter = function(){
-        document.querySelector("a").style.color="orange";
+        this.style.color="orange";
+    }
+    efeito[i].onmouseleave = function(){
+        this.style.color="grey";
     }
 }
-// efeito[1].onmouseleave = function(){
-//     document.querySelector("a").style.color="white";
-// }
+// opacidade
+for(let i=0;i<image.length;i++){
+    image[i].onmouseenter = function(){
+        this.style.opacity = "0.5";
+    }
+    image[i].onmouseleave = function(){
+        this.style.opacity = "1";
+    }
+}
+// botao entrar cores
+buttonEnter.onmouseenter = function(){
+    this.style.color="orange";
+}
+buttonEnter.onmouseleave = function(){
+    this.style.color="white";
+}
+//  botao cadastrar cores
+buttonCad.onmouseenter = function(){
+    this.style.color="black";
+}
+buttonCad.onmouseleave = function(){
+    this.style.color="white";
+}
+
